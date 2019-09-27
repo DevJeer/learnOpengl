7ce436538ce9f962f0c0a06aae2c1c7a79991e89
houjunfeng <18829536923@163.com>
2019-09-27 18:09:54 +0800

    添加特效头文件

--- 01_OpenglProjectTest/34.BreakOutGame/PostProcessor.h
+++ 01_OpenglProjectTest/34.BreakOutGame/PostProcessor.h
@@ -11,7 +11,26 @@
 
 class PostProcessor
 {
+public:
+	Shader PostProcessingShader;
+	Texture2D Texture;
+	GLuint Width, Height;
+	//Ñ¡Ôñ
+	GLboolean Confuse, Chaos, Shake;
+	PostProcessor(Shader shader, GLuint width, GLuint height);
 
+	void BeginRender();
+
+	void EndRender();
+
+	void Render(GLfloat time);
+
+private:
+	GLuint MSFBO, FBO;
+	GLuint RBO;
+	GLuint VAO;
+
+	void initRenderData();
 
 };
 
